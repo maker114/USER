f641318538669377e8e3dfb8f438b8536f9a71a2
Maker114 <2739305114@qq.com>
2024-09-10 18:34:42 +0800

    first commit

---
+++ main.c
@@ -0,0 +1,23 @@
+#include "delay.h"
+#include "sys.h"
+#include "usart.h"
+#include "Board.h"
+#include "WS2812.h"
+#include "Servo.h"
+
+int num = 0;
+int step = 2;
+
+int main(void)
+{
+	delay_init();
+	uart_init(115200);
+	Board_Init();
+	WS2812_init(8);
+	Servo_Init();
+	while (1)
+	{
+		Servo_SetAngle(170);
+		num += 1;
+	}
+}
